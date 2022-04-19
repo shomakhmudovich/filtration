90069253c97d771a0131726e3a261ed97db107a6
unknown <C:\Users\admin\AppData\Roaming\The Bat!>
2022-04-19 10:48:09 +0300

    HW

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Users from "./components/users";
+import API from "./API";
+
+const App = () => {
+    const [users, setUser] = useState(API.users.fetchAll());
+
+    const handleDeleteUser = (id) => {
+        setUser((prevState) => prevState.filter((user) => user._id !== id));
+    };
+    const handleToggleBookmark = (id) => {
+        setUser(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark };
+                }
+                return user;
+            })
+        );
+    };
+
+    return (
+        <>
+            <Users
+                users={users}
+                onDeleteUser={handleDeleteUser}
+                onToggleBookmark={handleToggleBookmark}
+            />
+        </>
+    );
+};
+
+export default App;
